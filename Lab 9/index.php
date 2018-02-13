dea4470e0727b77cd677a6eb475594a522c85388
haperezs <haperezs@hotmail.com>
2018-02-12 21:10:47 -0600

    Subir Lab 9 completo

--- Lab 9/index.php
+++ Lab 9/index.php
@@ -5,34 +5,34 @@
     function promedio($arr){
         $largo = count($arr);
         $sum = 0;
-        
+
         for($i = 0; $i < $largo ; $i++){
             $sum += $arr[$i];
         }
-        
+
         $promedio = $sum/$largo;
-        return $promedio; 
+        return $promedio;
     }
 
     function mediana($arr){
         $largo = count($arr);
         $aux = 0;
-        
+
         sort($arr);
-        
+
         if($largo % 2 == 0){
             $mediana = ([$arr[$largo/2], $arr[($largo/2)-1]])/2;
         }else{
             $mediana = $arr[$largo/2];
         }
-        
+
         return $mediana;
     }
 
     function menorMayor($arr){
         sort($arr);
         $largo = count($arr);
-        
+
         for($i = 0 ; $i < $largo ; $i++){
             echo "<li>" . $arr[$i] . "</li>";
         }
@@ -41,37 +41,51 @@
     function mayorMenor($arr){
         rsort($arr);
         $largo = count($arr);
-        
+
         for($i = 0 ; $i < $largo ; $i++){
             echo "<li>" . $arr[$i] . "</li>";
         }
     }
-    
+
     function lista($arr){
         echo "<ul><li>Promedio: " . promedio($arr) . "</li>";
         echo "<li>Mediana:" . mediana($arr) . "</li>";
-        
+
         echo "<li>Menor a mayor: </li>";
         menorMayor($arr);
-        
+
         echo "<li>Mayor a menor: </li>";
         mayorMenor($arr);
-        
+
         echo "</ul>";
     }
 
     function potencias($num){
         echo "<table><tr><th>Número</th><th>Cuadrado</th><th>Cubo</th></tr>";
-        
+
         for($i = 1; $i <= $num ; $i++){
             echo "<tr><td>" . $i . "</td><td>" . pow($i, 2) . "</td><td>" . pow($i, 3) . "</td></tr>";
         }
         echo "</table>";
     }
 
+    function binario($n){
+      $num = 0;
+      $res = 0;
+      $i = 1;
+
+    	while($n != 0){
+    		    $res = $n%2;
+            $n = floor($n/2);
+            $num = $num + ($res*$i);
+            $i = $i*10;
+    	}
+    	echo $num;
+    }
+
     function imprimeArray($arr){
         $largo = count($arr);
-        
+
         echo "[";
         for($i = 0 ; $i < $largo ; $i++){
             echo $arr[$i] . " ";
@@ -81,7 +95,7 @@
 
 
 
-    
+
     $arr1 = array(3, 2, 1, 1, 2, 3, 5, 4, 6);
     $arr2 = array(7, 3, 1, 7, 9);
 
@@ -92,7 +106,7 @@
     echo "Arreglo 2: <br>";
     imprimeArray($arr2);
     echo "<br>Promedio: " . promedio($arr2) . "<br>";
-    
+
 
     echo "<h3>Función 2</h3>" . "Arreglo 1:<br>";
     imprimeArray($arr1);
@@ -117,6 +131,13 @@
 
     echo "Num 7: <br>";
     echo potencias(7) . "<br>";
+
+
+    echo "<h3>Función 5</h3>" . "Convierte a binario<br>" . "Num 5:<br>";
+    echo binario(5) . "<br><br>";
+
+    echo "Num 9: <br>";
+    echo binario(9) . "<br>";
 
     include("_footer.html");
 ?>
--- Lab 9/preguntas.php
+++ Lab 9/preguntas.php
@@ -6,10 +6,10 @@
     echo "Muestra información sobre la configuración de PHP. Versión, módulos y opciones que compiló, etc.</li><br>";
 
     echo "<li>¿Qué cambios tendrías que hacer en la configuración del servidor para que pudiera ser apto en un ambiente de producción?<br>";
-    echo "</li><br>";
-         
+    echo "Cambiar el archivo php.ini para un ambiente de produccion (php.ini-production)</li><br>";
+
     echo "<li>¿Cómo es que si el código está en un archivo con código html que se despliega del lado del cliente, se ejecuta del lado del servidor? Explica.<br>";
     echo "Porque PHP despliega el HTML en el servidor y ya solo es necesario enviar al cliente el resultado</li></ul>";
-    
+
     include("_footer.html");
 ?>
